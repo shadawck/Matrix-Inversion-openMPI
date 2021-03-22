7cd7128556ccf294c2b4080456c0144d01a12124
remiflavien1 <hug211mire@gmail.com>
2021-03-22 16:11:01 +0100

    add algo with Bcast and reduce

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ void findPivot(size_t row, MatrixConcatCols &augmentedMatrix, size_t &pivot);
 
 void checkSingularity(const MatrixConcatCols &augmentedMatrix, size_t row, size_t pivot);
 
+void splitAugmentedMatrix(Matrix &mat, MatrixConcatCols &augmentedMatrix);
+
 using namespace std;
 int ROOT_PROCESS = 0;
 
@@ -50,6 +52,10 @@ void invertSequential(Matrix &mat) {
         }
     }
     // On copie la partie droite de la matrice AI ainsi transformée dans la matrice courante (this).
+    splitAugmentedMatrix(mat, augmentedMatrix);
+}
+
+void splitAugmentedMatrix(Matrix &mat, MatrixConcatCols &augmentedMatrix) {
     for (unsigned int i = 0; i < mat.rows(); ++i) {
         mat.getRowSlice(i) = augmentedMatrix.getDataArray()[slice(i * augmentedMatrix.cols() + mat.cols(), mat.cols(),
                                                                   1)];
@@ -59,8 +65,6 @@ void invertSequential(Matrix &mat) {
 void checkSingularity(const MatrixConcatCols &augmentedMatrix, size_t row, size_t pivot) {
     if (augmentedMatrix(pivot, row) == 0) throw runtime_error("Matrix not invertible");
 }
-
-double *convertValArrayToDouble(valarray<double> array);
 
 void findPivot(size_t row, MatrixConcatCols &augmentedMatrix, size_t &pivot) {
     pivot = row;
@@ -73,181 +77,75 @@ void findPivot(size_t row, MatrixConcatCols &augmentedMatrix, size_t &pivot) {
     }
 }
 
-valarray<double> convertDoubleToValArray(double *array, int arrayLength);
+struct {
+    double value;
+    int index;
+} send, recv;
 
 /** NAIVE IMPLEMENTATION
  * Inverser la matrice par la méthode de Gauss-Jordan; implantation MPI parallèle.
- * @param augmentedMatrix
+ * @param mat
  */
-void invertParallel(
-        Matrix &augmentedMatrix) { // Number of row staw matrixDimension but for augmentedMatrix col number change
+void invertParallel(Matrix &mat) {
     int rank, size;
-    assert(augmentedMatrix.rows() == augmentedMatrix.cols());
-    int rowSize = augmentedMatrix.rows();
-    int colSize = augmentedMatrix.rows();
+    size = MPI::COMM_WORLD.Get_size();
+    rank = MPI::COMM_WORLD.Get_rank();
 
-    MPI_Init(nullptr, nullptr);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    assert(mat.rows() == mat.cols());
+    MatrixConcatCols augmentedMatrix(mat, MatrixIdentity(mat.rows()));
 
-//    MatrixConcatCols *augmentedMatrix;
-
-    double *originalArray;
-    if (rank == 0) {
-//        augmentedMatrix = new MatrixConcatCols(augmentedMatrix, MatrixIdentity(augmentedMatrix.rows()));
-        cout << "PRINT AUGMENTED MATRIX" << endl;
-        cout << augmentedMatrix.str() << endl;
-        originalArray = convertValArrayToDouble(augmentedMatrix.getDataArray());
-    }
-
-    int numRows = rowSize / size;
-
-//    cout << "Array for rank : " << rank << endl;
-//    int subArrayLength = sizeof(subArray) / sizeof(subArray[0]);
-//    Matrix::printArray(subArray, subArrayLength);
-
-    double subArray[numRows * colSize];
-    if (size == 1) {
-        memcpy(subArray, originalArray, rowSize * colSize * sizeof(double));
-    } else {
-        for (int row = 0; row < numRows; row++) {
-            MPI_Scatter(&originalArray[row * colSize * size], colSize, MPI_DOUBLE,
-                        &subArray[row * colSize], colSize, MPI_DOUBLE, ROOT_PROCESS,
-                        MPI_COMM_WORLD);
-        }
-    }
-    /** DEBUG */
-    //cout << "Array for rank : " << rank << endl;
-    int subArrayLength = sizeof(subArray) / sizeof(subArray[0]);
-    //Matrix::printArray(subArray, subArrayLength);
-
-    double rowToSend[colSize];
-
-    // Get start time
-    double chronoStart;
-    if (rank == 0) {
-        chronoStart = MPI_Wtime();
-    }
-
-    int localRow, whichRank;
-    double pivot, scale;
-    for (int row = 0; row < rowSize; row++) {
-        Matrix::printArray(subArray, subArrayLength);
-        // row of the subMatrix
-        localRow = row / size;
-        // rank of this localRow
-        whichRank = row % size;
-
-        // if pivot in rank of localRow then eliminate
-        if (rank == whichRank) {
-            pivot = subArray[localRow * colSize + row];
-
-            // iterate through each element of row (without the pivot)
-            for (int j = row + 1; j < colSize; j++) {
-                double tmp = subArray[localRow * colSize + j];
-                subArray[localRow * colSize + j] /= pivot;
-                cout << "Before : " << tmp << "| After : " << subArray[localRow * colSize + j] << "| Pivot was :" << pivot << endl;
-            }
-
-            // directly assigne 1 to divided pivot
-            subArray[localRow * colSize + row] = 1;
-//            cout << endl << "Divided Row of rank : " << rank << endl;
-//            Matrix::printArray(subArray, subArrayLength);
-
-            // Copy the row into our send buffer
-            memcpy(rowToSend, &subArray[localRow * colSize], colSize * sizeof(double));
-
-            //cout << "Sended Row" << endl;
-            //Matrix::printArray(rowToSend, subArrayLength);
-
-            // Broadcast this row to all the ranks
-            MPI_Bcast(rowToSend, colSize, MPI_DOUBLE, whichRank, MPI_COMM_WORLD);
-
-            // Eliminate for the other rows mapped to this rank
-            for (int j = localRow + 1; j < numRows; j++) {
-                scale = subArray[j * colSize + row];
-
-                // Subtract to eliminate pivot from later rows
-                for (int k = row + 1; k < rowSize; k++) {
-                    double tmp = subArray[j * colSize + k];
-                    subArray[j * colSize + k] -= scale * rowToSend[k];
-                    cout << "Before : " << tmp << "| After : " << subArray[j * colSize + k] << "| Scale was :" << scale << "| rTS value was : " << rowToSend[k] << endl;
-                }
-
-                subArray[j * colSize + row] = 0;
-
-//                cout << endl << "UPDATED SUBARRAY of rank " << rank << endl;
-//                Matrix::printArray(subArray, subArrayLength);
-//                cout << endl;
-
-            }
-        } /*else {
-            // Receive a row to use for elimination
-            MPI_Bcast(rowToSend, colSize, MPI_DOUBLE, whichRank, MPI_COMM_WORLD);
-
-            for (int j = localRow; j < numRows; j++) {
-                if (whichRank < rank || j > localRow) {
-                    scale = subArray[j * colSize + row];
-                    //cout << "scale" << scale << endl;
-
-                    //Subtract to eliminate pivot from later rows
-                    for (int k = row + 1; k < rowSize; k++) {
-                        subArray[j * colSize + k] -= scale * rowToSend[k];
-                    }
-
-                    // Use assignment for the trivial elimination
-                    subArray[j * colSize + row] = 0;
+    for (size_t k = 0; k < augmentedMatrix.rows(); ++k) {
+        double lMax = 0;
+        size_t q = k;
+        for (size_t i = k; i < augmentedMatrix.rows(); ++i) {
+            if ((i % size) == rank) {
+                if (fabs(augmentedMatrix(i, k)) > lMax) {
+                    lMax = fabs(augmentedMatrix(i, k));
+                    q = i;
                 }
             }
-        }*/
-    }
+        }
+        send.index = q;
+        send.value = lMax;
+        MPI_Allreduce(&send, &recv, size, MPI_DOUBLE_INT, MPI_MAXLOC, MPI_COMM_WORLD); //recv.value is never used - change with MPI_recv and MPI_send
 
-    // Stop the time before the gather phase
-    double chronoEnd;
-    double total;
-    if (rank == 0) {
-        chronoEnd = MPI_Wtime();
-        total = chronoEnd - chronoStart;
-    }
+        q = recv.index;
+        int root = q % size;
+        MPI_Bcast(&augmentedMatrix(q, 0), augmentedMatrix.cols(), MPI_DOUBLE, root, MPI_COMM_WORLD);
 
-    /*
- * Collect all Sub-Matrices
- * All sub-matrices are gathered using the gather function
- */
+        checkSingularity(augmentedMatrix, q, k);
+
+        // on swap la ligne q avec la ligne k
+        if (q != k) augmentedMatrix.swapRows(q, k);
 
 
-    if (size == 1) {
-        memcpy(originalArray, subArray, rowSize * colSize * sizeof(double));
-    } else {
-        // Gather "size" rows at a time
-        for (int row = 0; row < numRows; row++) {
-            MPI_Gather(&subArray[row * colSize], colSize, MPI_DOUBLE,
-                       &originalArray[row * size * colSize], colSize, MPI_DOUBLE, ROOT_PROCESS,
-                       MPI_COMM_WORLD);
+        // on normalise la ligne k afin que l'element (k,k) soit egale a 1
+        double lValue = augmentedMatrix(k, k);
+        for (size_t j = 0; j < augmentedMatrix.cols(); ++j) {
+            augmentedMatrix(k, j) /= lValue;
+        }
+
+        //// Pour chaque rangée...
+        for (size_t i = 0; i < augmentedMatrix.rows(); ++i) {
+            if ((i % size) == rank) {
+                if (i != k) { // ...différente de k
+                    // On soustrait la rangée k
+                    // multipliée par l'élément k de la rangée courante
+                    double lValue = augmentedMatrix(i, k);
+                    augmentedMatrix.getRowSlice(i) -= augmentedMatrix.getRowCopy(k) * lValue;
+                }
+            }
+        }
+
+        for (int i = 0; i < augmentedMatrix.rows(); ++i) {
+            MPI_Bcast(&augmentedMatrix(i, 0), augmentedMatrix.cols(), MPI_DOUBLE, i % size, MPI_COMM_WORLD);
+        }
+
+        for (unsigned int i = 0; i < mat.rows(); ++i) {
+            mat.getRowSlice(i) = augmentedMatrix.getDataArray()[slice(
+                    i * augmentedMatrix.cols() + mat.cols(), mat.cols(), 1)];
         }
     }
-    MPI_Finalize();
-
-    if (rank == 0) {
-        auto val = convertDoubleToValArray(originalArray, sizeof(originalArray) / sizeof(originalArray[0]));
-        //cout << "FINAL ARRAY" << endl;
-        //Matrix::printValArray(val);
-
-        //cout << total << " Seconds" << endl;
-    }
-
-
-}
-
-// This is how you can get a dynamic array from a valarray.
-double *convertValArrayToDouble(valarray<double> array) {
-    auto *newArray = new double[array.size()];
-    copy(begin(array), end(array), newArray);
-    return newArray;
-}
-
-valarray<double> convertDoubleToValArray(double *array, int arrayLength) {
-    return valarray<double>(array, arrayLength);
 }
 
 // Multiplier deux matrices.
@@ -263,31 +161,67 @@ Matrix multiplyMatrix(const Matrix &iMat1, const Matrix &iMat2) {
 }
 
 int main(int argc, char **argv) {
-
     srand((unsigned) time(nullptr));
 
-    int matrixDimension = 5;
+    unsigned int matrixDimension = 5;
     if (argc == 2) {
         matrixDimension = atoi(argv[1]);
     }
+    int rank, size;
 
-    MatrixRandom lA(matrixDimension, matrixDimension);
+    MatrixRandom randomMatrix(matrixDimension, matrixDimension);
+    const Matrix &copyRandomMatrix(randomMatrix);
 
-    cout << "EXAMPLE" << endl;
-    MatrixExample matrixExample(matrixDimension, matrixDimension);
+    MPI_Init(nullptr, nullptr);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-//    cout << "Matrice random:\n" << lA.str() << endl;
+    /**
+    * sequential execution
+    */
+    // If Size = 1 : Execute sequential algo
+    if (rank == 0) {
+        double chronoSeqStart, chronoSeqEnd;
+        cout << "SEQUENTIAL EXECUTION" << endl;
+        Matrix seqMatrix(randomMatrix);
 
-    Matrix lB(lA);
-//    invertSequential(lB);
-//    cout << "Matrice inverse:\n" << lB.str() << endl;
-//
-//    Matrix lRes = multiplyMatrix(lA, lB);
-//    cout << "Produit des deux matrices:\n" << lRes.str() << endl;
-//
-//    cout << "Erreur: " << lRes.getDataArray().sum() - matrixDimension << endl;
+        chronoSeqStart = MPI_Wtime();
+        invertSequential(seqMatrix);
+        chronoSeqEnd = MPI_Wtime();
 
-    invertParallel(lA);
+        cout << "Matrice inverse:\n" << seqMatrix.str() << endl;
+        Matrix lRes = multiplyMatrix(seqMatrix, copyRandomMatrix);
+        cout << "Produit des deux matrices:\n" << lRes.str() << endl;
+
+        cout << "Erreur: " << lRes.getDataArray().sum() - matrixDimension << endl;
+        cout << "Total sequential execution time : " << chronoSeqEnd - chronoSeqStart << endl;
+    }
+
+    /**
+     * parallel execution
+     */
+
+    Matrix parMatrix = Matrix(randomMatrix);
+    MPI_Bcast(&parMatrix(0, 0), matrixDimension * matrixDimension, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    double chronoParStart = MPI_Wtime();
+    invertParallel(parMatrix);
+    double chronoParEnd = MPI_Wtime();
+
+    if (rank == 0) {
+        cout << "ORIGINAL MATRIX" << endl;
+        cout << copyRandomMatrix.str() << endl;
+
+        cout << "PARALLEL EXECUTION" << endl;
+        cout << "Matrice inverse:\n" << parMatrix.str() << endl;
+        Matrix lRes = multiplyMatrix(parMatrix, copyRandomMatrix);
+        cout << "Produit des deux matrices:\n" << lRes.str() << endl;
+
+        cout << "Erreur: " << lRes.getDataArray().sum() - matrixDimension << endl;
+        cout << "Total parallel execution time : " << chronoParEnd - chronoParStart << endl;
+    }
+
+    MPI_Finalize();
 
     return 0;
 }
